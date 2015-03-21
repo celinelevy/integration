7b78aea70a308499e6f4a26e148df310d744c19f
linecey <linecey35@gmail.com>
2015-03-21 11:53:08 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Business</title>
+    <!--<link rel="stylesheet" href="style.css">-->
+</head>
+<body>
+
+    
+
+/*-----------------------------
+   CELINE
+*/       
+    <header>
+        <div class="info">
+            <ul class="right">
+                <li><a href="#1" class="info-Num"></a></li>
+                <li><a href="#2" class="info-Mail"></a></li>
+                <li><a href="#3" class="info-Facebook"></a></li>
+            </ul>
+        </div>
+        
+        <div class="main-nav">
+            
+        </div>
+        
+    </header>
+
+
+ /*-----------------------------
+  NAME
+*/
+   
+   
+   
+   
+   
+    
+    
+    
+</body>
+</html>
